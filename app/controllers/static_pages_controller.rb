eb68c60821b64ce218728103e894e3fd30fcc9bf
Taras Bardiuk <taras.bardyuk@gmail.com>
2017-06-20 19:43:03 +0300

    Frontend using Bootstrap table

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @data = JSON.parse(File.read('data.json'))
+    @data = value_upcase(JSON.parse(File.read('data.json')))
     if params[:query]
-      query = params[:query].split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/)
+      query = params[:query].upcase.split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/)
       query.each do |search_word|
         @data = filter(@data, search_word)
       end
@@ -13,16 +13,31 @@ class StaticPagesController < ApplicationController
 
   def filter(data_array, word)
     matched = []
-    word = word.include?('"') ? word[1..-2].downcase : word.downcase
+    word = word[1..-2] if word.include?('"')
     data_array.each do |hash|
       if word.chars.first == '-'
-        unless hash.any? { |key, value| value.downcase.include? word[1..-1] }
-          matched << hash
+        unless hash.except(:relevance)
+                   .any? { |key, value| value.include? word[1..-1] }
+          matched << relevance(hash, word)
         end
-      elsif hash.any? { |key, value| value.downcase.include? word }
-        matched << hash
+      elsif hash.except(:relevance).any? { |key, value| value.include? word }
+        matched << relevance(hash, word)
       end
     end
-    matched
+    matched.sort_by { |k| [-k[:relevance], k['Name']] }
+  end
+
+  def relevance(hash, word)
+    hash[:relevance] = 0
+    hash[:relevance] += hash['Name'].include?(word) ? 3 :
+                          hash['Type'].include?(word) ? 2 :
+                            hash['Designed by'].include?(word) ? 1 : 0
+    hash
+  end
+
+  def value_upcase(data_array)
+    data_array.each do |hash|
+      hash.each { |k, v| v.upcase! }
+    end
   end
 end
